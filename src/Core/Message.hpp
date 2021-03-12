083b288ceff2f111a4a14a41ea633c9b419033e5
Aleksey Shein <a.shein@riftek.com>
2021-03-12 23:14:17 +0300

    add Messagectypes

--- src/Core/Message.hpp
+++ src/Core/Message.hpp
@@ -6,10 +6,12 @@
 namespace rf
 {
 
-template< typename T, const char*  typeIdStr > 
+template< typename T > 
 class Message : public IMessage
 {
 public:
+    static std::string TypeName(){ return std::string(typeName);}
+
     Message(uint64_t id = 0, uint64_t timestamp = 0);
     Message(uint64_t id, uint64_t timestamp, const T&  data);
     Message(uint64_t id, uint64_t timestamp, const T&& data);
@@ -18,57 +20,59 @@ public:
     void Set(uint64_t id, uint64_t timestamp, const T&& data);
     uint64_t Id() { return id; }
     uint64_t Timestamp() const { return timestamp; }
-    T &DataRef() { return Message; }
-    T GetData() { return Message; }
+    T &DataRef() { return data; }
+    T GetData() { return data; }
 
 protected:
 private:
+    static const char* typeName;//="Message";
     uint64_t id;
     uint64_t timestamp;
-    std::string typeId;
+    //std::string typeName;
     T data;
 };
 
-template< typename T, const char*  typeIdStr  > 
-Message<T,typeIdStr>::Message(uint64_t id_ = 0, uint64_t timestamp_ = 0)
+//std::string Message::TypeName { return std::string(typeName);}
+
+
+
+template< typename T  > 
+Message<T>::Message(uint64_t id_, uint64_t timestamp_)
 : id(id_)
 , timestamp(timestamp_)
-, typeId(typeIdStr)
 {
 }
 
-template< typename T, const char*  typeIdStr  > 
-Message<T,typeIdStr>::Message(uint64_t id_, uint64_t timestamp_, const T& profileQue_)
+template< typename T > 
+Message<T>::Message(uint64_t id_, uint64_t timestamp_, const T& data_)
 : id(id_)
 , timestamp(timestamp_)
-, profileQue(profileQue_)
-, typeId(typeIdStr)
+, data(data_)
 {
 }
 
-template< typename T, const char*  typeIdStr  > 
-Message<T,typeIdStr>::Message(uint64_t id_, uint64_t timestamp_, const T &&profileQue_)
+template< typename T > 
+Message<T>::Message(uint64_t id_, uint64_t timestamp_, const T &&data_)
 : id(id_)
 , timestamp(timestamp_)
-, profileQue(std::move(profileQue_))
-, typeId(typeIdStr)
+, data(std::move(data_))
 {
 }
 
-template< typename T, const char*  typeIdStr  > 
-void Message<T,typeIdStr>::Set(uint64_t id_, uint64_t timestamp_, const T &profileQue_)
+template< typename T > 
+void Message<T>::Set(uint64_t id_, uint64_t timestamp_, const T &data_)
 {
   id = id_
   timestamp = timestamp_;
-  profileQue = profileQue_;
+  data = data_;
 }
 
-template< typename T, const char*  typeIdStr  > 
-void Message<T,typeIdStr>::Set(uint64_t id_, uint64_t timestamp_, const T &&profileQue_)
+template< typename T > 
+void Message<T>::Set(uint64_t id_, uint64_t timestamp_, const T &&data_)
 {
   id = id_
   timestamp = timestamp_;
-  profileQue = profileQue_;
+  data = data_;
 }
 
 }// namespace rf
